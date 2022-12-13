3d23811a552cb739e3ccc725083d828dc4263e68
raymayo <ormillada.jr@gmail.com>
2022-12-14 02:32:06 +0800

    added PWA

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then(registration => {
+        console.log('SW Registered!');
+        console.log(registration)
+    }).catch(error => {
+        console.log('SW Registration Failed!');
+        console.log(error)
+    })
+}
+
+
+
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('static').then(cache => {
+            return cache.addAll(['./index.html', './style.css', './script.js', './img/120.png', './img/192.png', './img/512.png'])
+        })
+    )
+})
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response =>{
+            return response || fetch(e.request);
+        })
+    )
+});
