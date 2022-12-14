97b083e27ded28665d99a4f147795526bd7941f9
raymayo <ormillada.jr@gmail.com>
2022-12-14 13:18:12 +0800

    hello

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js').then(registration => {
         console.log('SW Registered!');
         console.log(registration)
+        console.log(nice);
     }).catch(error => {
         console.log('SW Registration Failed!');
         console.log(error)
@@ -63,7 +64,7 @@ startButton.addEventListener('click', function () {
                 
 
 
-				if (indicator === 0) {
+				if (indicator === 9) {
                     questionContainer.style.display = 'none'
                     question.textContent = '';
                     answerContainer.innerHTML = '';
